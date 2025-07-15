8aa99a65e290e836fc2a50b02a2d5837ad9f0e8f
Esteban Fernandez <estebantofi@gmail.com>
2025-07-15 01:47:40 -0300

    Deja de rastrear env.js y actualiza .gitignore

--- env.js
+++
@@ -1,4 +0,0 @@
-// env.js
-window.env = {
-  API_KEY_POKEMON: "45fd9bab-bb21-420d-aa3d-54e66acaeab7", //<---- Aca debes de poner tu API_KEY obtenida de https://dev.pokemontcg.io/ para funcionar en local.
-};
