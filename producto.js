04da495732770af7759a8f4a8dd07dbb150b5a31
Esteban Fernandez <estebantofi@gmail.com>
2025-07-15 20:54:46 -0300

    Se agrega un nav reutilizable, se elimina imagenes de prueba, se modifican estilos y se crea un loader.

--- producto.js
+++ producto.js
@@ -1,23 +1,33 @@
 const apiKey = window.env.API_KEY_POKEMON;
-fetch("https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:[1 TO 151]", {
-  headers: {
-    "X-Api-Key": `${apiKey}`,
-  },
-})
+const container = document.getElementById("container");
+// Loader
+const loader = document.createElement("div");
+loader.innerHTML = `<img src="img/loader.gif" alt="Cargando" style="width:200px;height:146px;margin-top:70%;" />
+  <div style="text-align:center;">Cargando cartas...</div>`;
+container.appendChild(loader);
+
+fetch(
+  "https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:[1 TO 151]",
+  {
+    headers: {
+      "X-Api-Key": `${apiKey}`,
+    },
+  }
+)
   .then((res) => res.json())
   .then((data) => {
-    const container = document.getElementById("container");
+      container.innerHTML = ""; // Limpiar container
     data.data.forEach((card) => {
       const cardHTML = `
         <div class="card">
           <img src="${card.images.small}" alt="${card.name}" />
-            <div class="card-info">   
+            <div class="card-info">
                 <div class="card-title">
                 ${card.name}
                 </div>
                 <div class="card-title">
                 ${"$" + card.cardmarket.prices.trendPrice || "-"}
-                </div> 
+                </div>
             </div>
           <hr style="width: 100%"/>
           <div class="card-desc">${
